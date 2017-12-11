829e436d515e4c30d346dcb8da69405803f92ed3
Luka Jankovic <lukjan1999@gmail.com>
2017-12-11 19:02:03 +0100

    Added uppercase B detection

--- inject.js
+++ inject.js
@@ -3,7 +3,8 @@
     var all = document.querySelectorAll("p,li,td,href,span");
 
     for (var i=0, max=all.length; i < max; i++) {
-	all[i].innerHTML = all[i].innerHTML.replace(/b/g, "🅱️")
+	     all[i].innerHTML = all[i].innerHTML.replace(/b/g, "🅱️")
+       all[i].innerHTML = all[i].innerHTML.replace(/B/g, "🅱️")
     }
 
 })();
