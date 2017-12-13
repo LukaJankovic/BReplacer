d4521b09d6959d10b181ee7c1e7bf6962efdc1d6
Luka Jankovic <lukjan1999@gmail.com>
2017-12-13 11:25:33 +0100

    Improved replace function

--- inject.js
+++ inject.js
@@ -1,10 +1,23 @@
+function textNodesUnder(node){
+  var all = [];
+  for (node=node.firstChild;node;node=node.nextSibling){
+    if (node.nodeType==3) all.push(node);
+    else all = all.concat(textNodesUnder(node));
+  }
+  return all;
+}
+
+
 (function() {
 
-    var all = document.querySelectorAll("p,li,td,href,span");
+    //var all = document.querySelectorAll("p,li,td,href,span");
+    var all = textNodesUnder(document.body)
+
+      console.log(all)
 
     for (var i=0, max=all.length; i < max; i++) {
-	     all[i].innerHTML = all[i].innerHTML.replace(/b/g, "🅱️")
-       all[i].innerHTML = all[i].innerHTML.replace(/B/g, "🅱️")
+	     all[i].textContent = all[i].textContent.replace(/b/g, "🅱️")
+       all[i].textContent = all[i].textContent.replace(/B/g, "🅱️")
     }
 
 })();
